aa144bb58adfcb1299945c115ea2304d21319362
Pei-Hung Lin <lin32@llnl.gov>
2022-03-15 15:10:43 -0700

    update tests

--- test/test2.c
+++ test/test2.c
@@ -1,15 +1,20 @@
 #include <stdlib.h>
  
 int main() {
-   int a;
+   int* a = malloc(sizeof(int) * 2);
+//   a[0] = 22;
+//   a[1] = 100;
+/*
    int b;
-   int *p=&a;
-   int *q=&a; // q is now an alias of p
+   int *p=a;
+   int *q=a; // q is now an alias of p
    int* r=0;
    if(rand()) // make sure both branches are analyzed
-        r=&a;
+        r=a;
     else
         r=&b;
     *r=100; // r points to a or b
     q = r;
+*/
+  return 0;
 }
